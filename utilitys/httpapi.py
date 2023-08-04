83582c133a1988169f1754f0b100561a4220b9b7
bad-youngster <should829@gmail.com>
2023-08-04 17:59:54 +0800

    add

---
+++ utilitys/httpapi.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# @Time  : 2023/08/04 17:09:15
+# @Author: wy
+from django.conf import settings
+import requests
+
+
+def nexus_api(method, uri, query_params, headers):
+    vm_url = settings.NEXUS_API + uri
+    try:
+        response = requests.request(method,
+                                    vm_url,
+                                    params=query_params,
+                                    headers=headers)
+        # Raise an exception if the request was unsuccessful
+        response.raise_for_status()
+        # Decode the response content as JSON, if applicable
+        try:
+            response_data = response.json()
+        except ValueError:
+            response_data = response.text
+
+        return response_data, response.status_code
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error occurred: {e}")
+        return None, None
